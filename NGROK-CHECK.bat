2a9dd66f22610e5169cc1a7ddf7061d366ab38b9
Idjdjsjsj <96491589+Idjdjsjsj@users.noreply.github.com>
2022-02-07 10:55:41 +0530

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -11,7 +11,7 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo User: Administrator
 echo Pass: @ka123456
 curl -O https://github.com/Idjdjsjsj/priyank/raw/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://github.com/Idjdjsjsj/priyank/blob/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://github.com/Idjdjsjsj/priyank/raw/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
